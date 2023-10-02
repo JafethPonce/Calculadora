6f3c2dd0b91b85b2a811a8bb454010e0fa5d8719
Christian Saucedo <21045144@alumno.utc.edu.mx>
2023-10-01 21:09:02 -0600

    Operaciones para la calculadora

--- js/script_calculadora.js
+++ js/script_calculadora.js
@@ -12,3 +12,62 @@ onOffButton.addEventListener('click', () => {
     showResult.textContent = '';
     showOp.classList.toggle('turn-on-off');
 });
+
+// Operaciones de la calculadora
+function calculator() {
+    document.addEventListener('click', e => {
+        let target = e.target;
+        if (target.tagName === "BUTTON") {
+            let cutedValue = target.value.trim();
+            switch (true){
+                case cutedValue == "equal":
+                    checkButtonValue(resultsCalc) ? printResult(resultsCalc) : 'ERROR SINTAXIS'; // verifying and displaying results
+                break;
+                case cutedValue == "clear": // if clear
+                    // clean everything
+                    resultsCalc.value = ''; 
+                    showResult.textContent = '';
+                    showOp.textContent = '';
+                break;
+                case cutedValue == "del":
+                    deleteCharacter(resultsCalc); // function -> delete back --> input hidden
+                    showOp.textContent = showOp.textContent.slice(0,-1); // delete char --> showOp
+                break;
+                case (cutedValue === "ln" 
+                || cutedValue === "exp" 
+                || cutedValue === "pow" 
+                || cutedValue === "log" 
+                || cutedValue === "ans" 
+                // add more operations
+                || cutedValue === "sq" 
+                || cutedValue === "sqrt" 
+                || cutedValue === "sin" 
+                || cutedValue === "cos"
+                || cutedValue === "tan"):
+                if (checkButtonValue(resultsCalc)) { 
+                    if (cutedValue === "ln") {
+                        cutedValue = "log";
+                    }
+                    // add more features
+                    else if (cutedValue === "pow") {
+                        resultsCalc.value = eval(resultsCalc.value) * eval(resultsCalc.value) * eval(resultsCalc.value);
+                        showOp.textContent = resultsCalc.value
+                    break;
+                    } 
+                    // <----------------> //
+                    else if (cutedValue === "sq") {
+                        resultsCalc.value = eval(resultsCalc.value) * eval(resultsCalc.value);
+                        showOp.textContent = resultsCalc.value
+                    break;
+                    } // default operation -->
+                    resultsCalc.value = eval(Math[cutedValue](resultsCalc.value));
+                    showOp.textContent = resultsCalc.value
+                }	
+                break;
+                default: // show function
+                    addDisplay(resultsCalc, target.value);	
+                    break;
+            }
+        }
+    });
+}
